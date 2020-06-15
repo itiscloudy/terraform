e9d75dc8d72708b603022cf904db8be2a1bca2ce
Sanjesh Kumar <skumar2@spscommerce.com>
2020-06-15 14:17:21 -0400

    Adding terraform file

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,11 @@
 provider "aws" {
-  profile    = "default"
+  alias="ss"
+  assume_role { role_arn = "arn:aws:iam::574124422856:role/terraformcloud" }
   region     = "us-east-1"
 }
 
 resource "aws_instance" "example" {
+  provider=aws.ss
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
 }
